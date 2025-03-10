524740356761779c72c6f15e471cf5908f385396
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2025-03-10 21:41:46 +0100

    Removed Sentry and fixed GitHub Pages deployment

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ea0a5848729c1201310a96d11289fa26@o4508909968097280.ingest.us.sentry.io/4508909974716416",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
